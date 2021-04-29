aca2442606721434a5b5cb46e1a42c83a177123b
jingjing20 <1269928993@qq.com>
2021-04-29 10:41:28 +0800

    update

--- music-admin-backend/app.js
+++ music-admin-backend/app.js
@@ -1,43 +1,48 @@
-const Koa = require('koa')
-const app = new Koa()
-const Router = require('koa-router')
-const router = new Router()
-    //跨域解决方法之一cors
-const cors = require('koa2-cors')
-    // 接受post请求参数解析
-const koaBody = require('koa-body')
+const Koa = require('koa');
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+//跨域解决方法之一cors
+const cors = require('koa2-cors');
+// 接受post请求参数解析
+const koaBody = require('koa-body');
 
-const ENV = 'jingjing-haohao'
+const config = require('./config'); // 一些配置信息可以放在 config.js 中，利用 dotenv 加载进来
+
+const ENV = 'jingjing-haohao';
 
 // 跨域
-app.use(cors({
-    origin: ['http://localhost:9528'],
-    credentials: true
-}))
+app.use(
+	cors({
+		origin: ['http://localhost:9528'],
+		credentials: true,
+	})
+);
 
 // 接受post请求参数解析
-app.use(koaBody({
-    multipart: true
-}))
+app.use(
+	koaBody({
+		multipart: true,
+	})
+);
 
-app.use(async(ctx, next) => {
-    console.log('jingjing 全局中间件')
-    ctx.state.env = ENV
-    await next()
-})
+app.use(async (ctx, next) => {
+	console.log('jingjing 全局中间件');
+	ctx.state.env = ENV;
+	await next();
+});
 
-const playlist = require('./controllers/playlist')
-const swiper = require('./controllers/swiper')
-const blog = require('./controllers/blog')
+const playlist = require('./controllers/playlist');
+const swiper = require('./controllers/swiper');
+const blog = require('./controllers/blog');
 
-router.use('/playlist', playlist.routes())
-router.use('/swiper', swiper.routes())
-router.use('/blog', blog.routes())
+router.use('/playlist', playlist.routes());
+router.use('/swiper', swiper.routes());
+router.use('/blog', blog.routes());
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-
-app.listen(8888, () => {
-    console.log('项目已启动在8888端口、、、')
-})
+app.listen(config.APP_PORT, () => {
+	console.log(`服务已启动在${config.APP_PORT}端口`);
+});
--- music-admin-backend/controllers/swiper.js
+++ music-admin-backend/controllers/swiper.js
@@ -1,70 +1,69 @@
-const Router = require('koa-router')
-const router = new Router()
-const callCloudDB = require('../utils/callCloudDB.js')
-const cloudStorage = require('../utils/callCloudStorage.js')
+const Router = require('koa-router');
+const router = new Router();
+const callCloudDB = require('../utils/callCloudDB.js');
+const cloudStorage = require('../utils/callCloudStorage.js');
 
-router.get('/list', async(ctx, next) => {
-    // 默认10条数据
-    const query = `db.collection('swiper').get()`
-    const res = await callCloudDB(ctx, 'databasequery', query)
-    console.log(res)
-        // 文件下载链接
-    let fileList = []
-    const data = res.data
-    for (let i = 0, len = data.length; i < len; i++) {
-        fileList.push({
-            fileid: JSON.parse(data[i]).fileid,
-            max_age: 7200
-        })
-    }
-    const dlRes = await cloudStorage.download(ctx, fileList)
-    console.log(dlRes)
-    let returnData = []
-    for (let i = 0, len = dlRes.file_list.length; i < len; i++) {
-        returnData.push({
-            download_url: dlRes.file_list[i].download_url,
-            fileid: dlRes.file_list[i].fileid,
-            _id: JSON.parse(data[i])._id
-        })
-    }
-    ctx.body = {
-        code: 20000,
-        data: returnData
-    }
-})
+router.get('/list', async (ctx, next) => {
+	// 默认10条数据
+	const query = `db.collection('swiper').get()`;
+	const res = await callCloudDB(ctx, 'databasequery', query);
+	console.log(res);
+	// 文件下载链接
+	let fileList = [];
+	const data = res.data;
+	for (let i = 0, len = data.length; i < len; i++) {
+		fileList.push({
+			fileid: JSON.parse(data[i]).fileid,
+			max_age: 7200,
+		});
+	}
+	const dlRes = await cloudStorage.download(ctx, fileList);
+	console.log(dlRes);
+	let returnData = [];
+	for (let i = 0, len = dlRes.file_list.length; i < len; i++) {
+		returnData.push({
+			download_url: dlRes.file_list[i].download_url,
+			fileid: dlRes.file_list[i].fileid,
+			_id: JSON.parse(data[i])._id,
+		});
+	}
+	ctx.body = {
+		code: 20000,
+		data: returnData,
+	};
+});
 
-router.post('/upload', async(ctx, next) => {
-    const fileid = await cloudStorage.upload(ctx)
-    console.log(fileid)
-        // 写数据库
-    const query = `
+router.post('/upload', async (ctx, next) => {
+	const fileid = await cloudStorage.upload(ctx);
+	console.log(fileid);
+	// 写数据库
+	const query = `
         db.collection('swiper').add({
             data: {
                 fileid: '${fileid}'
             }
         })
-    `
-    const res = await callCloudDB(ctx, 'databaseadd', query)
-    ctx.body = {
-        code: 20000,
-        id_list: res.id_list
-    }
-})
+    `;
+	const res = await callCloudDB(ctx, 'databaseadd', query);
+	ctx.body = {
+		code: 20000,
+		id_list: res.id_list,
+	};
+});
 
-router.get('/del', async(ctx, next) => {
+router.get('/del', async (ctx, next) => {
+	// 删除云数据库中的记录
+	const params = ctx.request.query;
+	const query = `db.collection('swiper').doc('${params._id}').remove()`;
+	const delDBres = await callCloudDB(ctx, 'databasedelete', query);
 
-    // 删除云数据库中的记录
-    const params = ctx.request.query
-    const query = `db.collection('swiper').doc('${params._id}').remove()`
-    const delDBres = await callCloudDB(ctx, 'databasedelete', query)
+	// 删除云存储中的文件
+	const delStorageRes = await cloudStorage.delete(ctx, [params.fileid]);
 
-    // 删除云存储中的文件
-    const delStorageRes = await cloudStorage.delete(ctx, [params.fileid])
-
-    ctx.body = {
-        code: 20000,
-        delDBres,
-        delStorageRes
-    }
-})
-module.exports = router
+	ctx.body = {
+		code: 20000,
+		delDBres,
+		delStorageRes,
+	};
+});
+module.exports = router;
